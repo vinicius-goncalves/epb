ce42f86a5c5f9a12e60fdf033e018eeb377b3e06
Vinícius Gonçalves <oldz.registros@gmail.com>
2024-08-26 15:37:42 -0300

    fix: incorrect value in indexOf query url

--- src/common/CCUtils/isWithinAThread.ts
+++ src/common/CCUtils/isWithinAThread.ts
@@ -1,5 +1,5 @@
 export function isWithinAThread(url: string | URL): boolean {
 	const u = new URL(url);
 	const href = u.href;
-	return href.indexOf('thread') > 0;
+	return href.indexOf('thread') > -1;
 }
