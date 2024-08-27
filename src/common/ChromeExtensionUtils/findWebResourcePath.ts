fdaee63c96ebb8c5ce0f27dbc73bc85375a1d227
Vinícius Gonçalves <oldz.registros@gmail.com>
2024-08-27 20:34:18 -0300

    refactor: improved code

--- src/common/ChromeExtensionUtils/findWebResourcePath.ts
+++ src/common/ChromeExtensionUtils/findWebResourcePath.ts
@@ -2,15 +2,19 @@ const manifest = chrome.runtime.getManifest();
 const webResources = manifest.web_accessible_resources;
 
 export function findWebResourcePath(value: string) {
-	if (webResources && Array.isArray(webResources)) {
-		for (const webResource of webResources) {
-			const wr = webResource as { resources: string[] };
-			if (Array.isArray(wr.resources)) {
-				for (const resource of wr.resources) {
-					if (resource.indexOf(value) > -1) {
-						return chrome.runtime.getURL(resource);
-					}
-				}
+	if (!webResources || !Array.isArray(webResources)) {
+		throw new Error('"Web Resources" property in manifest.json was not found or is not an array.');
+	}
+
+	for (const webResource of webResources) {
+		const wr = webResource as { resources: string[] };
+		const resources = wr.resources;
+
+		if (!Array.isArray(resources)) continue;
+
+		for (const resource of wr.resources) {
+			if (resource.indexOf(value) > -1) {
+				return chrome.runtime.getURL(resource);
 			}
 		}
 	}
