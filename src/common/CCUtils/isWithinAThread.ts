56f394a756a5374de071fd5d0ca46035701729a1
Vinícius Gonçalves <oldz.registros@gmail.com>
2024-08-27 12:43:33 -0300

    feat: added user preference

--- src/common/CCUtils/isWithinAThread.ts
+++ src/common/CCUtils/isWithinAThread.ts
@@ -1,4 +1,11 @@
-export function isWithinAThread(url: string | URL): boolean {
+import { UserPreferences } from '../classes/userPreferences/UserPreferences';
+
+const preferences = new UserPreferences();
+
+export async function isWithinAThread(url: string | URL): Promise<boolean | undefined> {
+	const { isPreferenceActive } = await preferences.getPreference('threadDetection');
+	if (!isPreferenceActive) return;
+
 	const u = new URL(url);
 	const href = u.href;
 	return href.indexOf('thread') > -1;
