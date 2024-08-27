0c27dbbf48c26e09503b3ea6131b1c24988b1319
Vinícius Gonçalves <oldz.registros@gmail.com>
2024-08-27 12:44:29 -0300

    fix: default forum returning "undefined" instead of "0"

--- src/common/ChromeExtensionUtils/getDefaultForumIndex.ts
+++ src/common/ChromeExtensionUtils/getDefaultForumIndex.ts
@@ -2,6 +2,6 @@ import Actions from '../../ts/enums/actions.enum';
 
 export function getDefaultForumIndex() {
 	return chrome.runtime.sendMessage({ action: Actions.GET_DEFAULT_FORUM }).then((res) => {
-		return res.default_forum;
+		return res.default_forum ?? 0;
 	});
 }
