68fc184dc29a086ab1dc8d9a060a990873cdd594
Vinícius Gonçalves <oldz.registros@gmail.com>
2024-08-29 22:15:23 -0300

    fix: debugConsole executing in the wrong context

--- src/pages/content/features/debugConsole.ts
+++ src/pages/content/features/debugConsole.ts
@@ -1,9 +1,15 @@
-import { UserPreferences } from '../../../common/classes/userPreferences';
+import { LocalUserPreferences, UserPreferences } from '../../../common/classes/userPreferences';
+import { getExtensionContext } from '../../../common/utils/getExtensionContext';
+const { ContextType } = chrome.runtime;
 
-const preferences = new UserPreferences();
+const preferences = new (
+	getExtensionContext() === ContextType.BACKGROUND ? UserPreferences : LocalUserPreferences
+)();
 
 export async function debugConsole(message: string, { epbPrefix = true }: { epbPrefix?: boolean } = {}) {
-	const preference = await preferences.getPreference('showDebugInformation');
+	const preference = (await preferences.getPreference('showDebugInformation')) as {
+		isPreferenceActive: boolean;
+	};
 
 	if (preference.isPreferenceActive) {
 		console.log(`${epbPrefix ? '[EPB]' : ''} ${message}`);
