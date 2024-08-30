d6797352a792ece64a7b4900e2b753ded985eec8
Vinícius Gonçalves <oldz.registros@gmail.com>
2024-08-29 22:18:33 -0300

    feat: user preferences load together content script and XHRInterceptor is also being injected with content script

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -1,10 +1,25 @@
 import '@assets/styles/tailwind.css';
 import './chrome/runtime/onMessage';
 
+import { findWebResourcePath } from '../../common/ChromeExtensionUtils';
+import { defaultPreferences, UserPreferences } from '../../common/classes/userPreferences';
+import { injectDOMScript } from '../../common/DOMUtils';
+import { Actions } from '../../ts/enums';
+
 import './features/initFeatures';
-import './features/lookForDefaultForum';
+import './features/lookForDefaultForum.feature';
+
+(() => {
+	const path = findWebResourcePath('XHRInterceptor');
+	injectDOMScript(path as string);
+
+	const userPreferences = new UserPreferences();
+	userPreferences.getPreferences().then((res) => {
+		localStorage.setItem('epb_user_preferences', JSON.stringify(res ?? defaultPreferences));
+	});
+})();
 
 window.addEventListener('EPBCustomResponse', ((ev: CustomEvent) => {
 	const response = ev.detail;
-	chrome.runtime.sendMessage({ action: 'epb-response', data: response });
+	chrome.runtime.sendMessage({ action: Actions.EPB_RESPONSE, data: response });
 }) as EventListener);
