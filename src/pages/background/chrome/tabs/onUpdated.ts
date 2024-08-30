c1fa98c10c29a93574d364cd9b18da3954e26f59
Vinícius Gonçalves <oldz.registros@gmail.com>
2024-08-29 22:14:10 -0300

    removed useless path

--- src/pages/background/chrome/tabs/onUpdated.ts
+++ src/pages/background/chrome/tabs/onUpdated.ts
@@ -1,9 +1,8 @@
 import handleExports from '../../handlers/handleExports.handler';
 import handleProfileEnhancement from '../../handlers/handleProfileEnhancement.handler';
-import { handleUserPreferences } from '../../handlers/handleUserPreferences.handler';
 import injectContentScript from '../injectContentScript';
 
-const funcs = [injectContentScript, handleExports, handleProfileEnhancement, handleUserPreferences];
+const funcs = [injectContentScript, handleExports, handleProfileEnhancement];
 
 chrome.tabs.onUpdated.addListener(() => {
 	for (const func of funcs) func();
